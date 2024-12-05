6c51cc1375c225049939e42d076cbe4d06a39796
XeivUPC <145395703+XeivUPC@users.noreply.github.com>
2024-12-05 09:53:14 +0100

    Update main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,1 +1,13 @@
 #include <Eigen/Dense>
+
+using namespace Eigen;
+
+Matrix3d RotationMatrixFromEulerAxisAndAngle(const Vector3d& axis, double angle) {
+	Vector3d normalizedAxis = axis.normalized();
+	AngleAxisd angleAxis = AngleAxisd(angle, axis);
+	angleAxis.toRotationMatrix();
+}
+
+int main() {
+	return 0;
+}
