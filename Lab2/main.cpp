02ba2853ec802997f3e69bd564bcb700252d7536
Ana Alcázar Cobo <ana.alcazar@estudiantat.upc.edu>
2024-12-05 10:31:09 +0100

    Update main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,13 +1,34 @@
 #include <Eigen/Dense>
-
+#include <math.h>
 using namespace Eigen;
 
-Matrix3d RotationMatrixFromEulerAxisAndAngle(const Vector3d& axis, double angle) {
+#define PI 2*acos(0.0)
+
+Matrix3d RotationMatrixFromEulerAxisAndAngle(const Vector3d& axis, double angle)
+{
 	Vector3d normalizedAxis = axis.normalized();
-	AngleAxisd angleAxis = AngleAxisd(angle, axis);
-	angleAxis.toRotationMatrix();
+	AngleAxisd angleAxis = AngleAxisd(angle, normalizedAxis);
+	return angleAxis.toRotationMatrix();
 }
 
-int main() {
+//Function to perform quaternion product
+Quaterniond QuaternionMultiplication(const Quaterniond& q1, const Quaterniond& q2)
+{
+	return q1 * q2;
+}
+
+int main()
+{
+	srand(static_cast<unsigned int> (time(0)));
+	for (size_t i = 0; i <= 100; i++)
+	{
+		int x = rand() % 100;
+		int y = rand() % 100;
+		int z = rand() % 100;
+
+		Vector3d axis = { x,y,z };
+		axis = axis.normalized();
+		RotationMatrixFromEulerAxisAndAngle({ x,y,z }, 6 * PI * i / 100);
+	}
 	return 0;
 }
