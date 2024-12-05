21d419a46647524a51dc33c14c98d9e49001c4b4
XeivUPC <145395703+XeivUPC@users.noreply.github.com>
2024-12-05 10:30:23 +0100

    new

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -2,12 +2,20 @@
 
 using namespace Eigen;
 
-Matrix3d RotationMatrixFromEulerAxisAndAngle(const Vector3d& axis, double angle) {
+Matrix3d EulerAxisAndAngle_ToRotationMatrix(const Vector3d& axis, double angle) {
 	Vector3d normalizedAxis = axis.normalized();
-	AngleAxisd angleAxis = AngleAxisd(angle, axis);
-	angleAxis.toRotationMatrix();
+	AngleAxisd angleAxis = AngleAxisd(angle, normalizedAxis);
+	return angleAxis.toRotationMatrix();
 }
 
 int main() {
+
+	Matrix3d resultMatrix = EulerAxisAndAngle_ToRotationMatrix({0,1,0}, 30);
+	double determinantValue = resultMatrix.determinant();
+	printf("%f\n", determinantValue);
+	if (resultMatrix.inverse() == resultMatrix.transpose()) {
+		printf("YES\n");
+	}
+
 	return 0;
 }
